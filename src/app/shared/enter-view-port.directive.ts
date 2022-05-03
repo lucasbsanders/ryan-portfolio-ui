cd0791a838607f305324b28bf8ad7e8467d08b2c
Lucas <lucasbsanders@gmail.com>
2022-05-02 20:37:17 -0700

    Remove commented code, stage index for PR

--- src/app/shared/enter-view-port.directive.ts
+++ src/app/shared/enter-view-port.directive.ts
@@ -12,22 +12,17 @@ import {
 export class EnterViewPortDirective {
 
   @Output() becomesVisible: EventEmitter<void> = new EventEmitter<void>();
-  //@Output() ratioChange: EventEmitter<number> = new EventEmitter<number>();
 
   private _observer: IntersectionObserver | undefined;
 
   constructor(@Host() private _elementRef: ElementRef) {}
 
   ngAfterViewInit(): void {
-    // for use with a sticky-top header
-    // const navHeight = <number>document.querySelector('.navbar')?.clientHeight * -1;
 
     const options = {
       root: null,
       rootMargin: '0px',
-      //rootMargin: `${navHeight}px 0px 0px 0px`,
       threshold: 0,
-      //threshold: [0, 0.1, 0.2, 0.3],
     };
 
     this._observer = new IntersectionObserver(this._callback, options);
@@ -41,7 +36,6 @@ export class EnterViewPortDirective {
 
   private _callback = (entries: any[], observer: any) => {
     entries.forEach((entry) => {
-      //this.ratioChange.emit(entry.intersectionRatio);
       if (entry.isIntersecting)
         this.becomesVisible.emit();
     });
