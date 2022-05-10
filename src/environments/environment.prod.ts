e3b3e2869a0aed11d340ebffa33de5f32ae140b7
Lucas <lucasbsanders@gmail.com>
2022-05-09 17:36:37 -0700

    Add script to generate environment files with secrets

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  s3: {
-    region: 'us-east-2',
-    identityPoolId: '',
-    baseUrl: 'https://s3.us-east-2.amazonaws.com',
-    bucketName: 'ryan-portfolio-bucket',
-  }
-};
