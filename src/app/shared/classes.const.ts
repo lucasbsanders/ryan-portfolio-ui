fa09b7d3b8c52ee0af20064791e84d390aa3fbee
Lucas <lucasbsanders@gmail.com>
2022-05-30 22:08:30 -0700

    Main updates

---
+++ src/app/shared/classes.const.ts
@@ -0,0 +1,31 @@
+import { SubtitleStyle, TileType, Width } from "./enums.const";
+
+export class PageDefault {
+  route = '';
+  type = '';
+  tiles = []
+};
+
+export class TileDefault {
+  order = 0;
+  type = TileType.Subtitle;
+  text = '';
+  html = '';
+  center = false;
+  width = Width.XL;
+  style = SubtitleStyle.Bottom;
+  images = [];
+  columns = 1;
+  hasFilters = false;
+};
+
+export class ImageDefault {
+  order = 0;
+  s3Key = '';
+  link = '';
+  url = '';
+  shadow = false;
+  overlay = false;
+  icon = false;
+  tags = [];
+};
