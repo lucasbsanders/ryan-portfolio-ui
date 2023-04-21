729baf984d330805d87b964edb8a136764d02df6
Lucas Sanders <lucasbsanders@gmail.com>
2023-04-21 11:35:25 -0700

    Spin and env update

--- src/app/shared/pipes/key-to-s3.pipe.ts
+++ src/app/shared/pipes/key-to-s3.pipe.ts
@@ -2,14 +2,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 @Pipe({
-  name: 'keyToS3'
+  name: 'keyToS3',
 })
 export class KeyToS3Pipe implements PipeTransform {
-
   transform(s3Key: string): string {
-    return (!s3Key || s3Key === '') ? 
-      environment.icons.primary : 
-      [environment.s3.baseUrl, environment.s3.bucketName, s3Key].join('/');
+    return !s3Key || s3Key === ''
+      ? environment.icons.primary
+      : [environment.s3.baseUrl, s3Key].join('/');
   }
-
 }
